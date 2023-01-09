732e881df10e5d29692cea86c6465bb7586047ea
NicBOMB <19939315+NicBOMB@users.noreply.github.com>
2023-01-09 11:05:18 -0800

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('script-extender-error-check', __dirname, 4);
+module.exports = webpack('script-extender-error-check', __dirname, 5);
 
